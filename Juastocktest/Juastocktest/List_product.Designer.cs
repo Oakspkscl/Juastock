9f1030d7beb518880d664c8bef16f1f2b76bd7c0
Oakspkscl <50256713+Oakspkscl@users.noreply.github.com>
2021-03-10 14:53:09 +0700

    Add project
    Add project

---
+++ Juastocktest/Juastocktest/List_product.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace Juastocktest
+{
+    partial class List_product
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(List_product));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.productDataSet = new Juastocktest.ProductDataSet();
+            this.tblproductBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_productTableAdapter = new Juastocktest.ProductDataSetTableAdapters.tbl_productTableAdapter();
+            this.productidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productstockDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productcostDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productpicDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblproductBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productidDataGridViewTextBoxColumn,
+            this.productnameDataGridViewTextBoxColumn,
+            this.productstockDataGridViewTextBoxColumn,
+            this.productcostDataGridViewTextBoxColumn,
+            this.productpicDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tblproductBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(178, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1081, 496);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(940, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 38);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(1048, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 38);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(1156, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 38);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(6, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 55);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "MENU";
+            // 
+            // button7
+            // 
+            this.button7.Image = global::Juastocktest.Properties.Resources.baseline_request_quote_black_18dp;
+            this.button7.Location = new System.Drawing.Point(16, 322);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(139, 55);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Image = global::Juastocktest.Properties.Resources.baseline_attach_money_black_18dp;
+            this.button6.Location = new System.Drawing.Point(16, 261);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(139, 55);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Image = global::Juastocktest.Properties.Resources.baseline_archive_black_18dp;
+            this.button5.Location = new System.Drawing.Point(16, 200);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 55);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = global::Juastocktest.Properties.Resources.baseline_shopping_cart_black_18dp;
+            this.button4.Location = new System.Drawing.Point(16, 139);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 55);
+            this.button4.TabIndex = 5;
+            this.button4.TabStop = false;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // productDataSet
+            // 
+            this.productDataSet.DataSetName = "ProductDataSet";
+            this.productDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblproductBindingSource
+            // 
+            this.tblproductBindingSource.DataMember = "tbl_product";
+            this.tblproductBindingSource.DataSource = this.productDataSet;
+            // 
+            // tbl_productTableAdapter
+            // 
+            this.tbl_productTableAdapter.ClearBeforeFill = true;
+            // 
+            // productidDataGridViewTextBoxColumn
+            // 
+            this.productidDataGridViewTextBoxColumn.DataPropertyName = "product_id";
+            this.productidDataGridViewTextBoxColumn.HeaderText = "product_id";
+            this.productidDataGridViewTextBoxColumn.Name = "productidDataGridViewTextBoxColumn";
+            this.productidDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // productnameDataGridViewTextBoxColumn
+            // 
+            this.productnameDataGridViewTextBoxColumn.DataPropertyName = "product_name";
+            this.productnameDataGridViewTextBoxColumn.HeaderText = "product_name";
+            this.productnameDataGridViewTextBoxColumn.Name = "productnameDataGridViewTextBoxColumn";
+            // 
+            // productstockDataGridViewTextBoxColumn
+            // 
+            this.productstockDataGridViewTextBoxColumn.DataPropertyName = "product_stock";
+            this.productstockDataGridViewTextBoxColumn.HeaderText = "product_stock";
+            this.productstockDataGridViewTextBoxColumn.Name = "productstockDataGridViewTextBoxColumn";
+            // 
+            // productcostDataGridViewTextBoxColumn
+            // 
+            this.productcostDataGridViewTextBoxColumn.DataPropertyName = "product_cost";
+            this.productcostDataGridViewTextBoxColumn.HeaderText = "product_cost";
+            this.productcostDataGridViewTextBoxColumn.Name = "productcostDataGridViewTextBoxColumn";
+            // 
+            // productpicDataGridViewTextBoxColumn
+            // 
+            this.productpicDataGridViewTextBoxColumn.DataPropertyName = "product_pic";
+            this.productpicDataGridViewTextBoxColumn.HeaderText = "product_pic";
+            this.productpicDataGridViewTextBoxColumn.Name = "productpicDataGridViewTextBoxColumn";
+            // 
+            // List_product
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 627);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "List_product";
+            this.Text = "List_product";
+            this.Load += new System.EventHandler(this.List_product_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblproductBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private ProductDataSet productDataSet;
+        private System.Windows.Forms.BindingSource tblproductBindingSource;
+        private ProductDataSetTableAdapters.tbl_productTableAdapter tbl_productTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productstockDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productcostDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productpicDataGridViewTextBoxColumn;
+    }
+}
---
+++ Juastocktest/Juastocktest/List_product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juastocktest
+{
+    public partial class List_product : Form
+    {
+        public List_product()
+        {
+            InitializeComponent();
+        }
+
+        private void List_product_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'productDataSet.tbl_product' table. You can move, or remove it, as needed.
+            this.tbl_productTableAdapter.Fill(this.productDataSet.tbl_product);
+
+        }
+    }
+}
